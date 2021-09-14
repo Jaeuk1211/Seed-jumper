2b2e9c3a4f9950de1cff5d3f4831f07c398c6130
LimSuH <perfectwhip0916@gmail.com>
2021-09-14 18:49:59 +0900

    마이페이지 & 정원 화면

--- SeedJumper/app/src/main/java/com/example/seedjumper/MainActivity.java
+++ SeedJumper/app/src/main/java/com/example/seedjumper/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.seedjumper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button myPage = findViewById(R.id.mypage);
+        Button garden = findViewById(R.id.garden);
+
+        myPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        garden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Garden.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
