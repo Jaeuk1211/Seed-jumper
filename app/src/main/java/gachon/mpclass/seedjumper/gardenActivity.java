475a22281370d504a3e978e16b822cb762297a75
LimSuH <perfectwhip0916@gmail.com>
2021-12-01 13:19:50 +0900

    정원아이템 배치조정중

--- app/src/main/java/gachon/mpclass/seedjumper/gardenActivity.java
+++ app/src/main/java/gachon/mpclass/seedjumper/gardenActivity.java
@@ -1,12 +1,18 @@
 package gachon.mpclass.seedjumper;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,35 +26,35 @@ public class gardenActivity extends AppCompatActivity {
     boolean i = true;
     ImageView plant;
     private RecyclerView mRecyclerView;
-    private ArrayList<plantObjectViewItem> mList;
-    private plantObjectViewAdapter mRecyclerViewAdapter;
     private String[] plantName = {"ddl", "rs", "tlp", "sf", "frs"};
     int count =0;
     private ImageView[] iv = new ImageView[9];
     int addIv = 0;
+    FrameLayout garden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden);
+        garden = findViewById(R.id.top_garden);
+        final int width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+        final int height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
 
-        plant = findViewById(R.id.plantCoordi1);
-
+        //plant = findViewById(R.id.plantCoordi1);
         mRecyclerView = (RecyclerView) findViewById(R.id.objects_listview);
-        mList = new ArrayList<>();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
 
-        ArrayList<plantObjectViewItem> list = new ArrayList<>();
+        plantObjectViewAdapter adapter = new plantObjectViewAdapter();
         for(int i = 0; i < 15; i++)
         {
             //파이어베이스에서 활성화된 아이템 목록 갖고오기
-            addItem(plantName[i % 5]);
+            adapter.addItem(plantName[i % 5]);
         }
 
-        mRecyclerViewAdapter = new plantObjectViewAdapter(mList);
-        mRecyclerView.setAdapter(mRecyclerViewAdapter);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+        adapter.notifyDataSetChanged();
+
+        mRecyclerView.setAdapter(adapter);
 
         ImageButton object = findViewById(R.id.change_object);
         object.setOnClickListener(new View.OnClickListener() {
@@ -60,11 +66,16 @@ public class gardenActivity extends AppCompatActivity {
             }
         });
 
-        mRecyclerViewAdapter.setOnItemClickListener(new plantObjectViewAdapter.OnItemClickListener(){
+        adapter.setItemClickListener(new OnItemClickListener(){
             @Override
-            public void onItemClick(View v, int pos){
-                Log.d("activity", "success!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                if(!mRecyclerViewAdapter.getName(pos).equals("noname") && count < 10){
+            public void OnItemClick(plantObjectViewAdapter.ViewHolder holder, View view, int pos){
+                DisplayMetrics metrics = new DisplayMetrics();
+                WindowManager windowManager = (WindowManager) getApplicationContext()
+                        .getSystemService(Context.WINDOW_SERVICE);
+                windowManager.getDefaultDisplay().getMetrics(metrics);
+                String name = adapter.getName(pos);
+                Toast.makeText(getApplicationContext(),"아이템 선택 " + name, Toast.LENGTH_SHORT).show();
+                if(!adapter.getName(pos).equals("noname") && count < 9){
                     for(int i =0; i< 9; i++)
                     {
                         if(iv[i] == null){
@@ -73,123 +84,139 @@ public class gardenActivity extends AppCompatActivity {
                     }
                     switch (pos){
                         case 0:
-                            iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                            iv[addIv] = new ImageView(view.getContext());
                             iv[addIv].setImageResource(R.drawable.seed6);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(380);
+                            iv[addIv].setY(480);
+                            garden.addView(iv[addIv]);
+
                             count ++;
                             break;
                         case 1:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.one_dandelion2);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(90);
+                            iv[addIv].setY(500);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 2:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.bundle_dandelion);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(700);
+                            iv[addIv].setY(520);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 3:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.seed1);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(380);
+                            iv[addIv].setY(300);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 4:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.one_rose);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(380);
+                            iv[addIv].setY(720);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 5:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.bundle_rose);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(240);
+                            iv[addIv].setY(610);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 6:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.seed2);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(500);
+                            iv[addIv].setY(690);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 7:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.one_tulip);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(480);
+                            iv[addIv].setY(400);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 8:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.bundle_whitetulip);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(220);
+                            iv[addIv].setY(390);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 9:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.seed3);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(590);
+                            iv[addIv].setY(590);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 10:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.one_sunflower);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(20);
+                            iv[addIv].setY(0);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 11:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.bundle_sunflower);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(20);
+                            iv[addIv].setY(0);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 12:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.seed4);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(20);
+                            iv[addIv].setY(0);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 13:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.one_yellowfreesia3);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(20);
+                            iv[addIv].setY(0);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                         case 14:
                             iv[addIv] = new ImageView(getApplicationContext());
-                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
                             iv[addIv].setImageResource(R.drawable.bundle_freesia);
-                            plant.setX(360);
-                            plant.setY(15);
+                            iv[addIv].setLayoutParams(new ViewGroup.LayoutParams(width, height));
+                            iv[addIv].setX(20);
+                            iv[addIv].setY(0);
+                            garden.addView(iv[addIv]);
                             count ++;
                             break;
                     }
@@ -207,11 +234,5 @@ public class gardenActivity extends AppCompatActivity {
 //            }
 //        });
 
-    }
-
-    public void addItem(String imgName){
-        plantObjectViewItem item = new plantObjectViewItem();
-        item.setPlantName(imgName);
-        mList.add(item);
     }
 }
